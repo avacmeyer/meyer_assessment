f14963d5f979caed073f17ba39c4ba5258d07fc8
Ava Meyer <avameyer@Avas-MBP.lan>
2023-04-21 02:26:37 -0700

    query for offers within age range and gender of current user

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -4,4 +4,10 @@ class Offer < ApplicationRecord
  
   validates_length_of :description, maximum: 255
   validates_presence_of :description, :start_date, :end_date 
+  
+  def self.target_offers
+    Offer.column_contains(:gender, user.gender)
+         .where(upper_bound: user.birthdate.., 
+                lower_bound: ..user.birthdate )
+  end
 end
