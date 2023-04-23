50990dac1570147380573db9aba470d4d98afd4e
Ava Meyer <avameyer@Avas-MBP.lan>
2023-04-22 21:53:02 -0700

    added tailwind tmeplates pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
-    @target_offers = Offer.target_offers(Current.user)
+    @target_offers = Offer.where("target_genders like ?", Current.user.gender)
+                          .where(upper_bound: Current.user.birthdate.., lower_bound: ..Current.user.birthdate )
     render inertia: 'Hello', props: {
       offers: @target_offers
     }
--- app/javascript/components/offers.js
+++ app/javascript/components/offers.js
@@ -2,16 +2,31 @@ import React from 'react'
 
 function Offers(props) {
   return (
-    <div>
-        <h1>Checkout these cool Offers</h1>
-        {props.offers.map((offer) =>{
-            return (
-              <div key={offer.id}>
-                <h3>{offer.description}</h3>
+    <div className="bg-white">
+       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+        <h2 className="text-2xl font-bold tracking-tight text-gray-900">Checkout these cool offers</h2>
+  
+        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          {props.offers.map((offer) => (
+            <div key={offer.id} className="group relative">
+              <div className="min-h-80 aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                <img
+                  src={offer.image}
+                  className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                />
               </div>
-            );
-        })}
-
+              <div className="mt-4 flex justify-between">
+                <div>
+                  <h3 className="text-sm text-gray-700">
+                    <span aria-hidden="true" className="absolute inset-0" />
+                    {offer.description}
+                  </h3>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
